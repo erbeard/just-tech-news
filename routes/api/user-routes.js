99738115d8ee2b519fcaa6a64b61f8d5cb3d5c9a
Ed Beard <ed.r.beard@gmail.com>
2022-10-25 10:45:30 -0400

    DB password update

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,14 +3,14 @@ const { User } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-  User.findAll({
-    attributes: { exclude: ['password'] }
-  })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    User.findAll({
+        attributes: { exclude: ['password'] }
+      })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
 });
 
 router.get('/:id', (req, res) => {
